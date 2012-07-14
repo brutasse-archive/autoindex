2e5d7bcbb6da09c574604cd925ad2304dc78d24e
Bruno Renié <buburno@gmail.com>
2012-07-14 17:39:27 +0200

    Initial package layout

---
+++ setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+from setuptools import find_packages
+
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+with open('requirements.txt') as reqs:
+    install_requires = [
+        line for line in reqs.read().split('\n') if (line and not
+                                                     line.startswith('--'))
+    ]
+
+setup(
+    name='autoindex',
+    version=__import__('autoindex').__version__,
+    author=u'Bruno Renié',
+    author_email='bruno@renie.fr',
+    packages=find_packages(),
+    include_package_data=True,
+    url='https://github.com/brutasse/autoindex',
+    license='BSD',
+    description='A partial PyPI mirror and private index server',
+    long_description=long_description,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+    zip_safe=False,
+    install_requires=install_requires,
+)
